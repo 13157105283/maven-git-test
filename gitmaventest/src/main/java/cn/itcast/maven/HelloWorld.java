a0d7993cda546f2c022c8c6bb966a0f686625d26
shenluoyang <1756634899@qq.com>
2019-04-02 14:37:05 +0800

    修改文件

--- gitmaventest/src/main/java/cn/itcast/maven/HelloWorld.java
+++ gitmaventest/src/main/java/cn/itcast/maven/HelloWorld.java
@@ -3,6 +3,7 @@ package cn.itcast.maven;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Git");
+        System.out.println("111111111111111");
         System.out.println("Git");
         System.out.println("Git");
         System.out.println("Git");
