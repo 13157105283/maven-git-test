6eecb8c02b8bd003b4b69f9294dbdc0f8cec6828
shenluoyang <1756634899@qq.com>
2019-04-02 14:24:49 +0800

    添加java工程测试

---
+++ gitmaventest/src/main/java/cn/itcast/maven/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.itcast.maven;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+        System.out.println("Git");
+
+    }
+}
